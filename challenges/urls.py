2aa726695011b0b078f6a24b7dbec78130a799bf
Ken Atopos <z1158229761@outlook.com>
2023-03-20 15:34:32 +0000

    add static files including css

--- challenges/urls.py
+++ challenges/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     # path("january", views.january),
     # path("february", views.february),
-    path("", views.index),
+    path("", views.index, name="index"),
     path("<int:month>", views.monthly_challenge_by_num),
     path("<str:month>", views.monthly_challenge, name="month-challenge")
 ]
