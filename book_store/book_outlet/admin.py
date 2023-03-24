d999a667b0dadb94429700f87a3f3dc5efcb9657
Ken Atopos <z1158229761@outlook.com>
2023-03-24 10:36:39 +0000

    add relationship to the database, namely one to many

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author
 
 # Register your models here.
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_filter = ("first_name", "last_name")
+    list_display = ("first_name", "last_name")
 
 
 class BookAdmin(admin.ModelAdmin):
@@ -12,3 +17,4 @@ class BookAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
