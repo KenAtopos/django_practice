8578a6ddeb54bc17f90904209ccdbbd4830d5fd2
Ken Atopos <z1158229761@outlook.com>
2023-03-24 11:27:27 +0000

    add one-to-one relationship

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Book, Author
+from .models import Book, Author, Address
 
 # Register your models here.
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_filter = ("city",)
 
 
 class AuthorAdmin(admin.ModelAdmin):
@@ -18,3 +22,4 @@ class BookAdmin(admin.ModelAdmin):
 
 admin.site.register(Book, BookAdmin)
 admin.site.register(Author, AuthorAdmin)
+admin.site.register(Address, AddressAdmin)
